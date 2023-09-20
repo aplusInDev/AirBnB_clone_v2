ac9febc0f66d5b9099dae9671f8cbe7fdcbdc0b5
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-20 15:55:42 +0100

    Removing a not-important line from the console module, updating the "all" method, and adding the "delete" method to the file_storage module

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,9 +20,19 @@ class FileStorage:
     # A private class attribute that stores all objects by <class name>.id
     __objects = {}
 
-    def all(self) -> dict:
+    def all(self, cls=None) -> dict:
         """Returns the dictionary __objects"""
+        if cls:
+            return {key: value for key, value in self.__objects.items()
+                    if isinstance(value, cls)}
         return self.__objects
+    
+    def delete(self, obj=None) -> None:
+        """Delete obj from __objects if it's inside"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
 
     def new(self, obj) -> None:
         """Sets in __objects the obj with key <obj class name>.id"""
