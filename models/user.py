aaceef11648aed4e44fa4e0cc7fa0022b768f270
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-20 23:25:03 +0100

    fixing task 7

--- models/user.py
+++ models/user.py
@@ -1,16 +1,28 @@
-#!/usr/bin/python3
-"""Define a class User"""
+#!/usr/bin/env python3
+"""This module defines a class called User
+that inherits from BaseModel and represents a user"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
+
+env_value = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class User(BaseModel, Base):
-    """User class"""
-    __tablename__ = 'users'
+class User(BaseModel):
+    """A class that inherits from BaseModel and represents a user"""
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    reviews = relationship('Review', backref='user', cascade='delete')
+    if env_value == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref='user', cascade="all, delete")
+        reviews = relationship("Review", backref='user', cascade="all, delete")
+    else:
+        email: str = ""
+        password: str = ""
+        first_name: str = ""
+        last_name: str = ""
