2971910a54031fa378f76a370b2e1e7fc871d231
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-20 17:03:49 +0100

    updating web_static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
 
-trap 'exit 0' ERR
-
-if ! command -v nginx &> /dev/null; then
-    sudo apt update
-    sudo apt install nginx -y
-fi
+sudo apt update
+sudo apt install nginx -y
 sudo mkdir -p "/data/web_static/releases/test/"
 sudo mkdir -p "/data/web_static/shared/"
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+oldstr="location / {"
+newstr="location /hbnb_static/ {\n\talias /data/web_static/current/;\n    }\n\n    $oldstr"
+sudo sed -i -z "s|$oldstr|$newstr|" /etc/nginx/sites-enabled/aplusdev.tech
+sudo nginx -t
 sudo service nginx restart
