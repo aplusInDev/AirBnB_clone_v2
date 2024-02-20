be7ccadf57f22e3a349e803ea197d9a0a96ce0b4
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-20 23:03:13 +0100

    updating task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,9 +5,6 @@ function do_deploy.
 from fabric.api import env, put, run
 from os.path import exists
 env.hosts = ['100.25.20.254', '54.144.154.99']
-# env.user = 'ubuntu'
-# env.key_filename = '~/.ssh/holberton'
-# env.warn_only = True
 
 
 def do_deploy(archive_path):
@@ -18,13 +15,13 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         folder_path = "/data/web_static/releases/" + archive_path[9:-4]
         run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{}.tgz -C {}/".format(
-            archive_path[9:-4], folder_path))
+        run("tar -xzf /tmp/{} -C {}/".format(
+            archive_path[9:], folder_path))
         run("rm /tmp/{}".format(archive_path[9:]))
         run("mv {}/web_static/* {}/".format(folder_path, folder_path))
         run("rm -rf {}/web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
         return True
-    except:
+    except Exception:
         return False
