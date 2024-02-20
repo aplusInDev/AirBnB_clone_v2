acf29f9b53fbd3b08a0206b564fed8bad6aab3eb
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-20 18:31:29 +0100

    fixing task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,8 @@ sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.htm
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 oldstr="location / {"
-newstr="location /hbnb_static/ {\n\talias /data/web_static/current/;\n    }\n\n    $oldstr"
+newstr="location /hbnb_static {\n\talias /data/web_static/current/;\n    }\n\n    $oldstr"
 sudo sed -i -z "s|$oldstr|$newstr|" /etc/nginx/sites-enabled/aplusdev.tech
-sudo nginx -t
+newstr="location =/hbnb_static {\n\talias /data/web_static/current/index.html;\n    }\n\n    $oldstr"
+sudo sed -i -z "s|$oldstr|$newstr|" /etc/nginx/sites-enabled/aplusdev.tech
 sudo service nginx restart
