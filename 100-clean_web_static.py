09abf96094d3fca6ac31081ee768b2032cabcfb6
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-21 00:18:15 +0100

    updating task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,10 +11,15 @@ env.hosts = ['100.25.20.254', '54.144.154.99']
 def do_clean(number=0):
     """Cleans up old versions of web_static"""
     number = int(number)
+    archive_count = local("ls -t versions | wc -l", capture=True)
     if number == 0 or number == 1:
         number = 1
-    else:
-        number += 1
-    local("ls -t versions | tail -n +{} | xargs rm -rf".format(number))
-    run("ls -t /data/web_static/releases | tail -n +{} | xargs rm -rf".format(
-        number))
+    unneccessary_archives = int(archive_count) - number
+    if unneccessary_archives > 0:
+        local("ls -t versions | tail -n +{} | xargs rm -rf".format(
+            unneccessary_archives))
+    releases_count = run("ls -t /data/web_static/releases | wc -l")
+    unneccessary_releases = int(releases_count) - number
+    if unneccessary_releases > 0:
+        run("ls -t /data/web_static/releases | tail -n +{} | xargs rm -rf"
+            .format(unneccessary_releases))
